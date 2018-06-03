b2c3047fc7402acf411b589f71aab08391582169
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2018-06-03 18:57:07 +0100

    Implemented dependency injection for the users system.

---
+++ delivery/base/src/main/kotlin/com/psa/kblog/di/UsersModule.kt
@@ -0,0 +1,26 @@
+package com.psa.kblog.di
+
+import com.psa.kblog.gateways.UsersGateway
+import com.psa.kblog.users.login.Login
+import com.psa.kblog.users.login.LoginInput
+import com.psa.kblog.users.logout.Logout
+import com.psa.kblog.users.logout.LogoutInput
+import com.psa.kblog.users.register.Register
+import com.psa.kblog.users.register.RegisterInput
+import dagger.Module
+import dagger.Provides
+
+@Module
+class UsersModule {
+    @Provides
+    fun provideLoginInteractor(usersGateway: UsersGateway): LoginInput =
+            Login(usersGateway)
+
+    @Provides
+    fun provideLogOutInteractor(usersGateway: UsersGateway): LogoutInput =
+            Logout(usersGateway)
+
+    @Provides
+    fun provideRegistrationInteractor(usersGateway: UsersGateway): RegisterInput =
+            Register(usersGateway)
+}
---
+++ delivery/base/src/test/kotlin/com/psa/kblog/di/UsersModuleSpec.kt
@@ -0,0 +1,26 @@
+package com.psa.kblog.di
+
+import com.nhaarman.mockito_kotlin.mock
+import com.psa.kblog.users.login.Login
+import com.psa.kblog.users.logout.Logout
+import com.psa.kblog.users.register.Register
+import io.kotlintest.matchers.beInstanceOf
+import io.kotlintest.should
+import io.kotlintest.specs.ShouldSpec
+
+class UsersModuleSpec: ShouldSpec({
+    should("return an interactor for log in") {
+        UsersModule().provideLoginInteractor(mock {  }) should
+                beInstanceOf(Login::class)
+    }
+
+    should("return an interactor for log out") {
+        UsersModule().provideLogOutInteractor(mock { }) should
+                beInstanceOf(Logout::class)
+    }
+
+    should("return a registration interactor") {
+        UsersModule().provideRegistrationInteractor(mock { }) should
+                beInstanceOf(Register::class)
+    }
+})
