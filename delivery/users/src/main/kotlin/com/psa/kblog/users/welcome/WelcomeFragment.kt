0ce400ef1a1b21c42b6ff5ed8967758ba315673a
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2018-08-04 20:50:23 +0100

    Added listeners for the actions in the welcome fragment.

--- delivery/users/src/main/kotlin/com/psa/kblog/users/welcome/WelcomeFragment.kt
+++ delivery/users/src/main/kotlin/com/psa/kblog/users/welcome/WelcomeFragment.kt
@@ -5,11 +5,19 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.psa.kblog.users.R
+import kotlinx.android.synthetic.main.welcome.*
 
 class WelcomeFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.welcome, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logIn.setOnClickListener { findNavController().navigate(R.id.loginAction) }
+        register.setOnClickListener { findNavController().navigate(R.id.registerAction) }
+    }
 }
