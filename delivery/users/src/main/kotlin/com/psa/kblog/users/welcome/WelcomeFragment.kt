99084eb8e6e16b92a430711362659e82c376e960
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2018-08-04 19:56:16 +0100

    Added view to welcome fragment.

--- delivery/users/src/main/kotlin/com/psa/kblog/users/welcome/WelcomeFragment.kt
+++ delivery/users/src/main/kotlin/com/psa/kblog/users/welcome/WelcomeFragment.kt
@@ -1,6 +1,15 @@
 package com.psa.kblog.users.welcome
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.psa.kblog.users.R
 
 class WelcomeFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.welcome, container, false)
 }
