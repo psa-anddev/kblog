8d2ceda7275d014bc98752f59f256ab624d1104f
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2018-08-19 15:18:47 +0100

    Created Blogs activity.

---
+++ delivery/blogs/src/main/kotlin/com/psa/kblog/blogs/BlogsActivity.kt
@@ -0,0 +1,12 @@
+package com.psa.kblog.blogs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BlogsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blogs)
+    }
+}
