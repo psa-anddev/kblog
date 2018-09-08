0199ab7af431a606817b8cf69a218e77672cf836
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2018-09-08 11:06:52 +0100

    Made blogs activity functional

--- delivery/blogs/src/main/kotlin/com/psa/kblog/blogs/BlogsActivity.kt
+++ delivery/blogs/src/main/kotlin/com/psa/kblog/blogs/BlogsActivity.kt
@@ -2,6 +2,7 @@ package com.psa.kblog.blogs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class BlogsActivity : AppCompatActivity() {
 
@@ -9,4 +10,7 @@ class BlogsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.blogs_activity)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+            findNavController(R.id.host).navigateUp()
 }
