6ea6e78fa3040227fb6dcb103c32634f786062a0
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2018-09-08 10:16:02 +0100

    Added view inflation to blog list fragment.

--- delivery/blogs/src/main/kotlin/com/psa/kblog/blogs/list/ListFragment.kt
+++ delivery/blogs/src/main/kotlin/com/psa/kblog/blogs/list/ListFragment.kt
@@ -1,6 +1,25 @@
 package com.psa.kblog.blogs.list
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.psa.kblog.blogs.R
+import kotlinx.android.synthetic.main.blogs_list.view.*
 
 class ListFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.blogs_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.blogs.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = BlogsAdapter()
+        }
+    }
 }
